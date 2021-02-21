87a7e14352570b83d172d86d647d7570698b58e4
Ashwin Chikkayya <ashwin.chikkayya@ness.com>
2021-02-21 10:39:41 +0530

    f001: Updated the plugin headers.

---
+++ wp-feature-cards.php
@@ -0,0 +1,21 @@
+/**
+ * Plugin Name
+ *
+ * @package           PluginPackage
+ * @author            Ashwin Kumar C
+ * @copyright         2021 Ashwin Kumar C
+ * @license           GPL-2.0-or-later
+ *
+ * @wordpress-plugin
+ * Plugin Name:       WP Feature Cards
+ * Plugin URI:        https://github.com/ashwinncj/wp-feature-cards
+ * Description:       Different Types of Feature Cards for the posts.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Ashwin Kumar C
+ * Author URI:        https://github.com/ashwinncj
+ * Text Domain:       plugin-slug
+ * License:           GPL v2 or later
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ */
