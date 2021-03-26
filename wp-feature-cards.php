c3228a72f19647a90c4b5b009a780c3555f54287
Ashwin Kumar C <ashwinncj@gmail.com>
2021-03-26 18:39:44 +0530

    Update wp-feature-cards.php

--- wp-feature-cards.php
+++ wp-feature-cards.php
@@ -19,4 +19,5 @@
  * Text Domain:       plugin-slug
  * License:           GPL v2 or later
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * GitHub Plugin URI: https://github.com/ashwinncj/wp-feature-cards
  */
